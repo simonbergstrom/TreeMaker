8d5abfc177632ef1372d4c5c64a4c9261396d892
simonberz <simbe109@gmail.com>
2014-12-16 09:00:22 +0100

    Some changes

--- main.js
+++ main.js
@@ -1,96 +1,88 @@
 
 
-// Main file with scene graph and all..
-var stats, scene, renderer;
-var camera, cameraControl;
+/*var clear = { clear: function(){
+canvas.width = canvas.width;}};*/
 
-
-if( !init() )	animate();
-
-// init the scene
-function init(){
-
-	if( Detector.webgl ){
-		renderer = new THREE.WebGLRenderer({
-			antialias		: true,	// to get smoother output
-			preserveDrawingBuffer	: true	// to allow screenshot
-		});
-		renderer.setClearColorHex( 0xBBBBBB, 1 );
-	// uncomment if webgl is required
-	//}else{
-	//	Detector.addGetWebGLMessage();
-	//	return true;
-	}else{
-		renderer	= new THREE.CanvasRenderer();
-	}
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.getElementById('container').appendChild(renderer.domElement);
-
-	// add Stats.js - https://github.com/mrdoob/stats.js
-	stats = new Stats();
-	stats.domElement.style.position	= 'absolute';
-	stats.domElement.style.bottom	= '0px';
-	document.body.appendChild( stats.domElement );
-
-	// create a scene
-	scene = new THREE.Scene();
-
-	// put a camera in the scene
-	camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.set(0, 0, 5);
-	scene.add(camera);
-
-	// create a camera contol
-	cameraControls	= new THREEx.DragPanControls(camera)
-
-	// transparently support window resize
-	THREEx.WindowResize.bind(renderer, camera);
-	// allow 'p' to make screenshot
-	THREEx.Screenshot.bindKey(renderer);
-	// allow 'f' to go fullscreen where this feature is supported
-	if( THREEx.FullScreen.available() ){
-		THREEx.FullScreen.bindKey();		
-		document.getElementById('inlineDoc').innerHTML	+= "- <i>f</i> for fullscreen";
-	}
-
-	// here you add your objects
-	// - you will most likely replace this part by your own
-	var geometry	= new THREE.TorusGeometry( 1, 0.42 );
-	var material	= new THREE.MeshNormalMaterial();
-	var mesh	= new THREE.Mesh( geometry, material );
-	mesh.rotation.y = Math.PI/2;
-	//scene.add( mesh );
-
-	new THREE.JSONLoader().load('teapot.js', function(geometry) { 
-	    var material	= new THREE.MeshNormalMaterial(); 
-	    var mesh	= new THREE.Mesh( geometry, material ); 
-	    scene.add( mesh ); 
-	    teapot = mesh; 
-	});
+function drawLine(x,y, x0,y0, color, width) {
+  ctx.beginPath();
+  ctx.moveTo(x,y);
+  ctx.lineTo(x0,y0);
+  ctx.strokeStyle = color;
+  if (params.constantWidth) ctx.lineWidth = 1; else
+  ctx.lineWidth = width;
+  ctx.stroke();
+}
+function getRandomColor() {
+  //return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+  var r = ~~( 255 * Math.random());
+  var g = ~~( 255 * Math.random());
+  var b = ~~( 255 * Math.random() );
+  var a = colors.alpha;
+  return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
 }
 
-// animation loop
+var camera, scene, renderer;
+var plant, mesh;
+var material;
+var tmp = new THREE.Vector3();
+
+init();
+animate();
+
+function init() {
+  // Basic settings for WebGL
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setSize(800,800); // Make Canvas 800x800
+  document.body.appendChild( renderer.domElement );
+
+  // Camera settings...
+  camera = new THREE.PerspectiveCamera( 45, 800 / 800, 1, 1000 );
+  camera.position.z = 300;
+  camera.position.y = 150;
+
+  // Scene settings..
+  scene = new THREE.Scene();
+  light2 = new THREE.PointLight( 0xffffff, 1, 100 );
+  light2.position.set(0, 100, -200);
+  scene.add( light2 );
+  directionalLight = new THREE.DirectionalLight( 0xffffff );
+  directionalLight.position.set(0, 0.5, -0.5);
+  directionalLight.position.normalize();
+  scene.add( directionalLight );
+  light2.position.y = 100;
+  light2.position.z = 100;
+
+  // Startrule for the L-system for Tree build
+  setRules0();
+
+  var material = new THREE.MeshBasicMaterial({color: 0x333333});
+  var line_geometry = new THREE.Geometry();
+  line_geometry = DrawTheTree(line_geometry, 0, -150, 0);
+  plant = new THREE.Line(line_geometry, material, THREE.LinePieces);
+  scene.add(plant);
+
+  renderer.setClearColor(0xeeeeee);
+  //window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function addTree(x,y){
+  var material = new THREE.MeshBasicMaterial({color: 0xaaa});
+  var line_geometry = new THREE.Geometry();
+  line_geometry = DrawTheTree(line_geometry, x, y, 0);
+  scene.add(new THREE.Line(line_geometry, material, THREE.LinePieces));
+  scene.add(new THREE.MorphAnimMesh(line_geometry, material));
+}
+
 function animate() {
-
-	// loop on request animation loop
-	// - it has to be at the begining of the function
-	// - see details at http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-	requestAnimationFrame( animate );
-
-	// do the render
-	render();
-
-	// update stats
-	stats.update();
+  requestAnimationFrame( animate );
+  //t0 = Date.now() / 60;
+  //scene.rotation.y = t0;
+  plant.rotation.y += 0.001;
+  camera.lookAt(plant.position);
+  renderer.render( scene, camera );
 }
 
-// render the scene
-function render() {
-
-	// update camera controls
-	cameraControls.update();
-	teapot.rotation.y += 0.01; 
-
-	// actually render the scene
-	renderer.render( scene, camera );
+window.onkeypress = function(e) {
+  e = e || window.event;
+  if(e.keyCode == 87) params.deltarota +=1;
 }
